1a5cc01fd7546761f67ecf5cc8313dce1f1455b9
Franz Knupfer <franzknupfer@hotmail.com>
2020-02-03 15:44:22 -0800

    add firebase connection

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 import { createStore } from 'redux';
-// import reducer from './reducers/ticket-list-reducer';
 import { Provider } from 'react-redux';
 import rootReducer from './reducers/index';
 
