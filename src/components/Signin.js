f5c89b3e42c3493da7cf2a6f362868a711551fbf
Franz Knupfer <franzknupfer@hotmail.com>
2020-02-13 16:47:57 -0800

    final commit for day

--- src/components/Signin.js
+++ src/components/Signin.js
@@ -1,6 +1,9 @@
 import React from "react";
+import firebaseui from "firebaseui";
+import firebase from "firebase/app";
 
 function Signin(){  
+  const ui = new firebaseui.auth.AuthUI(firebase.auth());
   return (
     <h1>Sign In</h1>
   );
